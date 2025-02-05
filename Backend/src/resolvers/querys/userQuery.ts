58a052a569ab5a14133fd963f2f60570fc3f0baa
jimmy <lettevoueto.jimmy@gmail.com>
2025-02-05 15:19:39 +0100

    modularisation query

---
+++ Backend/src/resolvers/querys/userQuery.ts
@@ -0,0 +1,80 @@
+import { DataSourceContext } from "../../context";
+import { QueryResolvers } from "../../types";
+
+export const getUser: QueryResolvers["getUser"] = async (
+  _,
+  __,
+  { dataSources }: DataSourceContext
+) => {
+  const user = await dataSources.db.user.findMany();
+  return user;
+};
+
+export const getUserByName: QueryResolvers["getUserByName"] = async (
+  _,
+  { username },
+  { dataSources }: DataSourceContext
+) => {
+  try {
+    const user = await dataSources.db.user.findFirst({
+      where: { username },
+    });
+
+    if (!user) {
+      return {
+        code: 400,
+        message: "Utilisateur introuvable",
+        success: false,
+        username: null,
+      };
+    }
+
+    return {
+      code: 201,
+      message: "Utilisateur trouvé",
+      success: true,
+      username: user.username,
+    };
+  } catch (error) {
+    return {
+      code: 500,
+      message: "Une erreur est survenue",
+      success: false,
+      username: null,
+    };
+  }
+};
+
+export const getUserById: QueryResolvers["getUserById"] = async (
+  _,
+  { id },
+  { dataSources }: DataSourceContext
+) => {
+  try {
+    const user = await dataSources.db.user.findUnique({
+      where: { id },
+    });
+    if (!user) {
+      return {
+        code: 400,
+        message: "Utilisateur introuvable",
+        success: false,
+        user: null,
+      };
+    }
+
+    return {
+      code: 201,
+      message: "Utilisateur trouvé",
+      success: true,
+      user: user,
+    };
+  } catch (error) {
+    return {
+      code: 500,
+      message: "Une erreur est survenue",
+      success: false,
+      user: null,
+    };
+  }
+};
