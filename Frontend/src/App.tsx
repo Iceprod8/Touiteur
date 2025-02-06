dfef9460215011400919749814df92063ecc79fe
Gina Palesch <gpalesch@bynativ.com>
2025-02-06 16:36:45 +0100

    handleLogin

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -43,7 +43,7 @@ function App() {
   const handleLogin = () => {
     setIsLoggedIn(true);
     setUsername(username);
-    navigate('/register');
+    navigate('/');
   };
   const handleLogout = () => {
     logout();
